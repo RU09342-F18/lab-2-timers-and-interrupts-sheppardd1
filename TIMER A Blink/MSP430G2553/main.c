06e2fb1693e22a76b69192909b9ee2e9440021bb
David <sheppardd1@students.rowan.edu>
2018-09-28 20:50:35 -0400

    fixed bugs

--- TIMER A Blink/MSP430G2553/main.c
+++ TIMER A Blink/MSP430G2553/main.c
@@ -72,13 +72,18 @@
 #define LED1 BIT6   //defining LED1 as BIT6
 #define BUTTON BIT3 //defining BUTTON as BIT3
 
+int Hz_to_timer(int);
+
+int time;
+
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  P1DIR |= 0x01;                            // P1.0 output
+  P1DIR |= 0x41;                            // P1.0 output
   CCTL0 = CCIE;                             // CCR0 interrupt enabled
-  CCR0 = 50000;                            //set capture compare register
-  TACTL = TASSEL_2 + MC_2 + ID_2;           // SMCLK, up mode, input divider = 4
+  time = Hz_to_timer(2);
+  CCR0 = time;                            //set capture compare register
+  TACTL = TASSEL_1 + MC_1 + ID_1;           // ACLK, up mode, input divider = 4
 
   __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
 }
@@ -92,12 +97,16 @@ void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A (void)
 #else
 #error Compiler not supported!
 #endif
-//if (specific CCR/Timer goes off)
 {
-  P1OUT ^= LED0;                            // Toggle P1.0
-  CCR0 += 50000;                            // Add Offset to CCR0
+  P1OUT ^= (LED0 + LED1);                            // Toggle P1.0
+  CCR0 = time;                            // Add Offset to CCR0
 }
-//else if(different CCR/timer goes off){ blink other LED }
-
 // ACLK is 32768 Hz
 
+int Hz_to_timer(int Hz)
+{
+   if(Hz <= 32768)
+       return (32768 / Hz);
+   else
+       return 1;
+}
